7db6c2c6344793e56334686c404619165be3e24d
nitu <nitujaiswal8271@gmail.com>
2023-03-12 15:40:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from "axios"
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   const[image,setimage]=useState("")
+   const[result,setResult]= useState([])
+    const onChange=(event)=>{
+    var newResult= event.target.value
+    setimage(newResult)
+   }
+   const changephoto=()=>{
+    axios.get(`https://api.unsplash.com/search/photos?page=1&query=${image}&client_id=L3sVAhXec88drVp_pVrJbqQbCCsO68zYcrRLxgtQKww`)
+    .then((res)=>{
+setResult(res.data.results)
+})
+   }
 
+  return (
+    <>
+    <div className="App">
+      <h1>Photo-Search-App</h1>
+      <input type="text" value={image} placeholder='type something' style={{ margin: "20px" }}  onChange={onChange}/>
+      <button  type="submit" onClick={changephoto} className="btn btn-primary" >get image</button>
+    </div>
+    <div className="container">
+                <div className="row text-center text-lg-start">
+                    {result.map((value) => {
+                        return (
+                            <div class="col-lg-3 col-md-4 col-6">
+                                    <img className="img-fluid img-thumbnail d-block mb-4 h-100" src={value.urls.small} alt='' />
+                            </div>
+                        )
+                    })}
+                </div>
+
+            </div>
+    </>
+  );
+};
 export default App;
+//L3sVAhXec88drVp_pVrJbqQbCCsO68zYcrRLxgtQKww
